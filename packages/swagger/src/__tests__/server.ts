2a30e0a7276bbc99d11c0b00af364a5945dfd58e
yurikrupnik <krupnik.yuri@gmail.com>
2021-06-23 21:09:46 +0300

    trying to add swagger test

--- packages/swagger/src/__tests__/server.ts
+++ packages/swagger/src/__tests__/server.ts
@@ -1,6 +1,37 @@
+// import express from "express";
 // import request from "supertest";
-// import app from "../server";
+// import swagger from "../index";
+//
+// const app = express();
+//
+// app.use(
+//     swagger(
+//         "https://app.swaggerhub.com/apis/yurikrupnik/backed-api_optional_string/1.0.0",
+//         ""
+//     )
+// );
 
+// test("Get swagger ui", (done) => {
+//     request(app)
+//         .get("/doc")
+//         // .expect("Content-Type", /json/)
+//         // .expect("Content-Length", "4")
+//         .expect(200, done);
+//     // .end((err, res) => {
+//     //     if (err) throw err;
+//     // });
+// });
+
+// test("Get json file", (done) => {
+//     request(app)
+//         .get("swagger.json")
+//         // .expect("Content-Type", /json/)
+//         // .expect("Content-Length", "4")
+//         .expect(200, done);
+//     // .end((err, res) => {
+//     //     if (err) throw err;
+//     // });
+// });
 test("Tes", () => {
     expect(1).toBe(1);
 });
