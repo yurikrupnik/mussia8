b101e4a3abfa1570ce769a967c874895896de602
yurikrupnik <krupnik.yuri@gmail.com>
2021-06-25 20:23:29 +0300

    added models

--- packages/models/src/index.ts
+++ packages/models/src/index.ts
@@ -2,7 +2,6 @@ import User, { UserGroupFront, UsersSchema } from "./user";
 
 export * as Project from "./project";
 // export * as User from "./user";
-// export default () => {return 'ds'};
 
 export default User;
 export { UsersSchema };
