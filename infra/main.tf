130b07c37788ebce3c99bbd9d3b109e7ca2a45fe
yurikrupnik <krupnik.yuri@gmail.com>
2021-08-14 15:01:55 +0300

    updated packages

--- infra/main.tf
+++ infra/main.tf
@@ -4,7 +4,7 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = "3.78.0"
+      version = ">=3.79.0"
     }
     null = {
       source  = "hashicorp/null"
